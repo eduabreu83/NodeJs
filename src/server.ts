130c0dfea7531d57ab25736cf06a32819300a20a
eduabreu83 <eduardoabreu83@gmail.com>
2025-05-19 23:20:17 -0300

    at

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+/*
+este foi criado sem o express e sem o typescript
+
 import {createServer} from 'node:http';
 
 const server = createServer((req, res) => {
@@ -5,5 +8,17 @@ const server = createServer((req, res) => {
 });
 
 server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
+*/
+
+//criando o servidor com express
+import express from 'express';
+
+const server = express();
+server.get('/', (req, res) => {
+  res.send('Eu estou aprendendo essa porra');
+});
+server.listen(3000, () => { 
   console.log('Server is running at http://localhost:3000');
 });
